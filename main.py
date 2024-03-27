53390f533e9a136003c17e10a59a0c1fb348d8f9
Kyle64Bit2602 <142840056+Kyle64Bit2602@users.noreply.github.com>
2024-03-27 13:10:39 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    #calls all other functions
+
+def get_shift():
+    #prompts the user for the shift value
+    #and return the value as a string
+    
+def choose_option():
+    #prompts the user to encode or decode
+    #returns True for encode or False for decode
+
+def get_message():
+    #prompts the user to enter a message to encode/decode
+    #returns the message
+    
+def create_key(shift):
+    #accepts the shift value from get_shift
+    #creates the cipher according to the shift value and store the key
+    #in a dictionary and return the dictionary as the key
+    
+def encode(message, key):
+    #encode accepts message as a string and key as a dictionary
+    #encodes the message using the key and return the
+    #encoded message a string
+
+def decode(message, key):
+    #decode accepts message as a string and key as a dictionary
+    #encodes the message using the key and return the
+    #encoded message a string
